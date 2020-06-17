9f377897068118327db40381de93d6480744d479
Paul Craig <paul_craig_16@hotmail.com>
2020-06-17 16:59:18 -0400

    Soften password validations
    It's a demo app for the moment so we want it to be easy to create
    & update user passwords.

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -88,13 +88,10 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'OPTIONS': {
+            'min_length': 3,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
