8019920a154f5faaa6269ab8980b6804ea9a63af
Stéphane Boisvert <stboisvert@gmail.com>
2020-06-18 17:58:55 -0400

    add example env variables
    Pull from env variables for postgres

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'covid',
+        'USER': os.getenv('POSTGRES_USERNAME'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
