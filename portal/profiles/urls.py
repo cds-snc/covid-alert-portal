7e9c8bfbab837bdf982f2bff82defaf7481dc493
Paul Craig <paul_craig_16@hotmail.com>
2020-06-18 12:18:59 -0400

    Added placeholder pages for /start and /code

--- portal/profiles/urls.py
+++ portal/profiles/urls.py
@@ -1,20 +1,23 @@
 from django.urls import path, include, re_path
 from django.conf.urls import url
-from django.views.generic import RedirectView
+from django.views.generic import RedirectView, TemplateView
 
 from . import views
 
 
 urlpatterns = [
+
     path('', RedirectView.as_view(pattern_name='start')),
-    re_path(r'start/$', views.StartPageView.as_view(), name='start'),
+    path('start/', TemplateView.as_view(template_name="profiles/start.html"), name='start'),
+    path('code/', TemplateView.as_view(template_name="profiles/code.html"), name='code'),
+    path('profiles/', views.StartPageView.as_view(), name='profiles'),
     re_path(r'signup/$', views.SignUp.as_view(), name='signup'),
     url(
-        r'users/(?P<pk>\w+)$',
+        r'profiles/(?P<pk>\w+)$',
         views.UserProfileView.as_view(),
         name='user_profile',
     ),
-    url(r'users/(?P<pk>\w+)/edit$', views.UserEdit.as_view(), name='user_edit'),
+    url(r'profiles/(?P<pk>\w+)/edit$', views.UserEdit.as_view(), name='user_edit'),
 
     # default auth routes: https://docs.djangoproject.com/en/3.0/topics/auth/default/#module-django.contrib.auth.views
     # /login/ [name='login']
