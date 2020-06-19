75c65739eb236e2e6f8e9a522ed4f19d8e21a3c5
Stéphane Boisvert <stboisvert@gmail.com>
2020-06-18 21:50:59 -0400

    Add static files folder
    We need this to push to Heroku. Let's also gitignore it

--- portal/portal/settings.py
+++ portal/portal/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'profiles.HealthcareUser'
