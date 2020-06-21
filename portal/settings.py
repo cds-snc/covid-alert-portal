f1743ddffd864e29b9ccd9e43d7086eef151b98f
Paul Craig <paul_craig_16@hotmail.com>
2020-06-21 13:42:05 -0400

    Make security key into an environment variable

--- portal/settings.py
+++ portal/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j$e+wzxdum=!k$bwxpgt!$(@6!iasecid^tqnijx@r@o-6x%6d'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY',
+                       'j$e+wzxdum=!k$bwxpgt!$(@6!iasecid^tqnijx@r@o-6x%6d')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
