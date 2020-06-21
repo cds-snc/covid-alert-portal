12000b3b8cecf14a9053395297ab94dd88684684
Paul Craig <paul_craig_16@hotmail.com>
2020-06-21 19:09:09 -0400

    Write 2 basic tests for our 2 basic views
    - Check the /start loads with 200 and the h1 is what we expect
    - Check the /code page loads with 200 and the code is there

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.urls import reverse
+
+
+class StartView(TestCase):
+    def test_start(self):
+        """
+        Just see the start page
+        """
+        response = self.client.get(reverse('start'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,
+                            "Generate code for Exposure Notification app")
+
+
+class CodeView(TestCase):
+    def test_code(self):
+        """
+        Just see the code page and one code
+        """
+        response = self.client.get(reverse('code'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response,
+                            "Provide patient with code")
+        self.assertContains(response,
+                            "<code id=\"big-code\">{}</code>".format(response.context['code']))
