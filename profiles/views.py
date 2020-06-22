2871de55a61baf31cadd03bb730e9e6ef4dace85
Stéphane Boisvert <stboisvert@gmail.com>
2020-06-22 19:29:17 -0400

    Delete all profile views

--- profiles/views.py
+++ profiles/views.py
@@ -2,57 +2,9 @@ import requests
 import os
 import sys
 
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.shortcuts import render, get_object_or_404
-from profiles.models import HealthcareUser
-from django.views.generic import View, ListView
+
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.urls import reverse_lazy
-from django.views import generic
-
-from .forms import SignupForm, HealthcareUserEditForm
-
-
-class UserListView(LoginRequiredMixin, ListView):
-    model = HealthcareUser
-
-class UserProfileView(LoginRequiredMixin, View):
-    def get(self, request, pk):
-        try:
-            user = HealthcareUser.objects.get(id=pk)
-        except:
-            user = None
-
-        context = {
-            "viewed_user": user
-        }
-
-        return render(request, "profiles/user_profile.html", context)
-
-
-class UserEdit(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
-    model = HealthcareUser
-    form_class = HealthcareUserEditForm
-    template_name = 'profiles/user_edit.html'
-    login_url = '/login/'
-    redirect_field_name = 'redirect_to'
-
-    def test_func(self):
-        # the logged-in user should be the same as the user ID
-        # the id is an int, so we cast it to a string
-        return str(self.request.user.id) == self.kwargs['pk']
-
-    def get_initial(self):
-        user = get_object_or_404(HealthcareUser, pk=self.kwargs['pk'])
-        # pre-populate the form
-        return {
-            'email': user.email,
-            'name': user.name,
-            'language': user.language,
-        }
-
-    def get_success_url(self):
-        return reverse_lazy('user_profile', kwargs={'pk': self.kwargs['pk']})
 
 @login_required
 def code(request):
