eee8131e4b6928523d321ca6d070edbfa44d0518
Stéphane Boisvert <stboisvert@gmail.com>
2020-06-22 19:23:01 -0400

    Remove access to profiles

--- profiles/urls.py
+++ profiles/urls.py
@@ -11,22 +11,16 @@ urlpatterns = [
     path('', TemplateView.as_view(template_name="profiles/homepage.html"), name='homepage'),
     path('code/', views.code, name='code'),
     path('start/', login_required(TemplateView.as_view(template_name="profiles/start.html")), name='start'),
-    path('profiles/', views.UserListView.as_view(), name='profiles'),
-    url(
-        r'profiles/(?P<pk>\w+)$',
-        views.UserProfileView.as_view(),
-        name='user_profile',
-    ),
-    url(r'profiles/(?P<pk>\w+)/edit$', views.UserEdit.as_view(), name='user_edit'),
 
-    # default auth routes: https://docs.djangoproject.com/en/3.0/topics/auth/default/#module-django.contrib.auth.views
-    # /login/ [name='login']
-    # /logout/ [name='logout']
-    # /password_change/ [name='password_change']
-    # /password_change/done/ [name='password_change_done']
-    # /password_reset/ [name='password_reset']
-    # /password_reset/done/ [name='password_reset_done']
-    # /reset/<uidb64>/<token>/ [name='password_reset_confirm']
-    # /reset/done/ [name='password_reset_complete']
+    # Removed for now
+    # path('profiles/', views.UserListView.as_view(), name='profiles'),
+    # url(
+    #     r'profiles/(?P<pk>\w+)$',
+    #     views.UserProfileView.as_view(),
+    #     name='user_profile',
+    # ),
+    # url(r'profiles/(?P<pk>\w+)/edit$', views.UserEdit.as_view(), name='user_edit'),
+
     path('', include('django.contrib.auth.urls')),
 ]
+
