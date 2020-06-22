92fdf78237b06b90c9c06b229374c5206e7a870c
Paul Craig <paul_craig_16@hotmail.com>
2020-06-22 11:49:58 -0400

    Compile the CSS and copy the static files in entrypoint.sh
    During the "development" build, we don't need to do this, but by
    default, we run with DEBUG=False, so let's compile the CSS beforehand.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,13 +4,13 @@
 echo "Apply database migrations"
 python manage.py migrate --noinput
 
-# If running in 'production' mode, we would need these
+# If running in 'production' mode, we need these
 
 # echo "Compile CSS"
-# python manage.py sass profiles/assets/scss/ profiles/static/css/
+python manage.py sass profiles/assets/scss/ profiles/static/css/
 
 # echo "Collect static files"
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 # Start server
 echo "Starting server"
