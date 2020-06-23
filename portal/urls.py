e43a1c6106506f3dfd369b2cee824a7e9864d575
Paul Craig <paul_craig_16@hotmail.com>
2020-06-23 13:15:22 -0400

    Add i18n path prefixes to our URLs
    Includes all of our auth URLs as well.

--- portal/urls.py
+++ portal/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n')),
-    path('', include('profiles.urls')),
 ]
+
+urlpatterns += i18n_patterns(
+    path('', include('profiles.urls'))
+)
