acf09bb513fc667bf41d69546b0d015931c67097
Stéphane Boisvert <stboisvert@gmail.com>
2020-06-23 15:09:01 -0400

    use actual boolean

--- portal/settings.py
+++ portal/settings.py
@@ -189,5 +189,6 @@ EMAIL_HOST = os.getenv('EMAIL_HOST')
 EMAIL_PORT = os.getenv('EMAIL_PORT')
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+email_use_tls = os.getenv('EMAIL_USE_TLS')
+EMAIL_USE_TLS = True if email_use_tls == 'True' else False
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
