b3f3aa0c9f8ed220700aebbfa228b3783df466ec
Paul Craig <paul_craig_16@hotmail.com>
2020-06-25 00:58:18 -0400

    Add error styling and heavily modify login template
    Got the inputs and errors all looking pretty good, as well as
    adding an ErrorList element at the top of the page.
    
    Got lots of help along the way:
    - Django field rendering: https://docs.djangoproject.com/en/3.0/topics/forms/#rendering-fields-manually
    - GOV.UK error messages: https://design-system.service.gov.uk/components/error-message/
    - GOV.UK example form: https://design-system.service.gov.uk/patterns/bank-details/
    - Stackoverflow, get django error list: https://stackoverflow.com/questions/2035288/getting-a-list-of-errors-in-a-django-form
    - Stackoverflow, remove autofocus in forms: https://stackoverflow.com/questions/21515605/remove-autofocus-attribute-from-field-in-django

--- profiles/forms.py
+++ profiles/forms.py
@@ -4,19 +4,34 @@ from django.contrib.auth.forms import (
     AuthenticationForm,
 )
 from django import forms
+from django.core.validators import EmailValidator
+from django.utils.translation import gettext_lazy as _
 
 from .models import HealthcareUser
 
 
 class HealthcareAuthenticationForm(AuthenticationForm):
+    """
+    A login form extending the Django default AuthenticationForm.
+    https://github.com/django/django/blob/9a54a9172a724d38caf6a150f41f23d79b9bdbb7/django/contrib/auth/forms.py#L173
+    """
+
     class Meta:
         model = HealthcareUser
-        fields = ["username", "password"]
 
     # override field attributes: https://stackoverflow.com/a/56870308
     def __init__(self, *args, **kwargs):
         kwargs.setdefault("label_suffix", "")
         super(HealthcareAuthenticationForm, self).__init__(*args, **kwargs)
+
+        # remove autofocus from fields
+        for field in self.fields:
+            self.fields[field].widget.attrs.pop("autofocus", None)
+
+        # update / translate validation message for invalid emails
+        self.fields["username"].validators = [
+            EmailValidator(message=_("Enter a valid email address"))
+        ]
 
 
 class SignupForm(UserCreationForm):
