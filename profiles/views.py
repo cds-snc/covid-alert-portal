e6dffbf311ae2d7d364df5cc72f726aa840afa3a
Stéphane Boisvert <stboisvert@gmail.com>
2020-06-26 10:49:11 -0400

    Remove unused import

--- profiles/views.py
+++ profiles/views.py
@@ -2,13 +2,10 @@ import requests
 import os
 import sys
 
-
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
 from .forms import SignupForm
 from django.contrib import messages
-from django.utils.html import escape,strip_tags
-
 
 
 
