8117f77e209f5d8b1c30ba530c6075b8d29f5b8e
Stéphane Boisvert <stboisvert@gmail.com>
2020-06-28 14:44:13 -0400

    Remove unused import

--- profiles/views.py
+++ profiles/views.py
@@ -6,8 +6,6 @@ import sys
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 
-from django.urls import reverse_lazy
-from django.views import generic
 from django.utils import timezone
 from .forms import SignupForm
 from django.contrib import messages
