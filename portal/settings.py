38876a1c2ebf65b341c2a3d9553a391afb633a38
Stéphane Boisvert <stboisvert@gmail.com>
2020-06-27 23:16:46 -0400

    Update settings.py
    Add better EMAIL defaults

--- portal/settings.py
+++ portal/settings.py
@@ -182,9 +182,9 @@ INVITATIONS_INVITATION_ONLY = True
 # Email setup
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-email_use_tls = os.getenv("EMAIL_USE_TLS")
+email_use_tls = os.getenv("EMAIL_USE_TLS", "True")
 EMAIL_USE_TLS = True if email_use_tls == "True" else False
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
