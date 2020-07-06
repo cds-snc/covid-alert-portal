05f68bf80f337c1797490d88f5af9f0dd445b411
Paul Craig <paul_craig_16@hotmail.com>
2020-07-06 14:23:35 -0400

    Add robots.txt disallowing crawlers
    Because Django serves static files from the "/static" directory,
    putting a file in there would end up as "/static/robots.txt", which
    isn't want we want. (It needs to be served from the root.)
    
    These rules disallow us from being indexed.
    
    Source:
    - https://www.robotstxt.org/robotstxt.html
    - https://adamj.eu/tech/2020/02/10/robots-txt/

--- portal/urls.py
+++ portal/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.http import HttpResponse
 from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
+    re_path(
+        r"^robots.txt",
+        lambda x: HttpResponse("User-Agent: *\nDisallow: /", content_type="text/plain"),
+        name="robots_file",
+    ),
     path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
 ]
