3d64f1f0cb87af0dd48446e7d9850ffc9cb9cdf4
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-07-06 13:12:11 -0400

    Created function with pw validation check

--- profiles/management/commands/createdefaultsuperuser.py
+++ profiles/management/commands/createdefaultsuperuser.py
@@ -1,19 +1,23 @@
 import os
-from django.core.exceptions import ValidationError
+from portal.settings import CREATE_DEFAULT_SU, SU_DEFAULT_PASSWORD, AUTH_PASSWORD_VALIDATORS
+from django.contrib.auth.password_validation import validate_password
 from django.core.management.base import BaseCommand, CommandError
 from profiles.models import HealthcareUser
 
-CREATE_SU = os.getenv("DJANGO_DEFAULT_SU", False) == "True"
-SU_PASSWORD = os.getenv("DJANGO_DEFAULT_SU_PASSWORD")
 SU_EXISTS = HealthcareUser.objects.filter(name="hcw_portal_admin").exists()
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        if CREATE_SU and not SU_PASSWORD == "" and not SU_EXISTS:
-            HealthcareUser.objects.create_superuser("hcw_portal_admin@cds-snc.ca","hcw_portal_admin", SU_PASSWORD)
-            self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
-        elif HealthcareUser.objects.filter(name="hcw_portal_admin").exists():
+        if CREATE_DEFAULT_SU and SU_DEFAULT_PASSWORD and not SU_EXISTS:
+            self.create_su()
+        elif SU_EXISTS:
             self.stdout.write(self.style.SUCCESS('Default Super User already exists'))
         else:
           self.stdout.write(self.style.SUCCESS('Not creating default Super User'))
+
+    def create_su(self):
+        ## Check to ensure SU password meets minimum requirements
+        if validate_password(SU_DEFAULT_PASSWORD) == None:
+            HealthcareUser.objects.create_superuser("hcw_portal_admin@cds-snc.ca","hcw_portal_admin", SU_DEFAULT_PASSWORD)
+            self.stdout.write(self.style.SUCCESS('Successfully created new super user'))
