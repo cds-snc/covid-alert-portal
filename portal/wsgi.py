c621e3fad8f8a538a6ffc4d7fde420a46ae0ad58
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-07-07 13:19:28 -0400

    Added newrelic config import

--- portal/wsgi.py
+++ portal/wsgi.py
@@ -6,7 +6,11 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
+import newrelic.agent
+newrelic.agent.initialize()
+# Uses the following env vars to set config
+# NEW_RELIC_LICENSE_KEY
+# NEW_RELIC_APP_NAME 
 import os
 
 from django.core.wsgi import get_wsgi_application
