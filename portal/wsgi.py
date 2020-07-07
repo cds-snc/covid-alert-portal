9a9ba2ea3ab65ccce38abf812f1571a7b62c43c6
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-07-07 13:31:34 -0400

    linting change

--- portal/wsgi.py
+++ portal/wsgi.py
@@ -7,14 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 import newrelic.agent
+import os
+from django.core.wsgi import get_wsgi_application
 
 newrelic.agent.initialize()
 # Uses the following env vars to set config
 # NEW_RELIC_LICENSE_KEY
 # NEW_RELIC_APP_NAME
-import os
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
 
