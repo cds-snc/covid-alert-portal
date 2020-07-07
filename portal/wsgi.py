1b09a1d52bf64394b2a3ab74c60308fa6cf74301
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-07-07 17:20:07 -0400

    Add config require for New Relic monitoring (#63)
    * Added newrelic config import
    
    * Add import requirement i pipfile
    
    * formatting change
    
    * linting change
    
    * Commit to re-activate github actions
    
    Co-authored-by: Stéphane <stephane.boisvert@cds-snc.ca>

--- portal/wsgi.py
+++ portal/wsgi.py
@@ -6,10 +6,14 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
+import newrelic.agent
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+newrelic.agent.initialize()
+# Uses the following env vars to set config
+# NEW_RELIC_LICENSE_KEY
+# NEW_RELIC_APP_NAME
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "portal.settings")
 
