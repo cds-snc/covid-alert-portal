a4c85f0722463408d072f8fa8fcbb22aa744c147
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-07-07 13:26:29 -0400

    formatting change

--- portal/wsgi.py
+++ portal/wsgi.py
@@ -7,10 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 import newrelic.agent
+
 newrelic.agent.initialize()
 # Uses the following env vars to set config
 # NEW_RELIC_LICENSE_KEY
-# NEW_RELIC_APP_NAME 
+# NEW_RELIC_APP_NAME
 import os
 
 from django.core.wsgi import get_wsgi_application
