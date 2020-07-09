1ccb1c60e4f2d037e6ebc4c3893e84fac62c1abd
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-07-09 11:21:27 -0400

    Change to su default name and heroku config

--- profiles/management/commands/createdefaultsuperuser.py
+++ profiles/management/commands/createdefaultsuperuser.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         if SU_EXISTS:
             self.stdout.write(self.style.SUCCESS("Default Super User already exists"))
-        elif CREATE_DEFAULT_SU and SU_DEFAULT_PASSWORD and not SU_EXISTS:
+        elif CREATE_DEFAULT_SU and SU_DEFAULT_PASSWORD:
             self.create_su()
         else:
             self.stdout.write(self.style.SUCCESS("Not creating default Super User"))
@@ -22,6 +22,6 @@ class Command(BaseCommand):
         # https://docs.djangoproject.com/en/3.0/topics/auth/passwords/#django.contrib.auth.password_validation.validate_password
         if validate_password(SU_DEFAULT_PASSWORD) is None:
             HealthcareUser.objects.create_superuser(
-                "hcw_portal_admin@cds-snc.ca", "hcw_portal_admin", SU_DEFAULT_PASSWORD
+                "admin@cds-snc.ca", "hcw_portal_admin", SU_DEFAULT_PASSWORD
             )
             self.stdout.write(self.style.SUCCESS("Successfully created new super user"))
