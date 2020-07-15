76c7e61312660ed703121f25d3248e766b4565e9
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-07-15 16:48:04 -0400

    lint formatting

--- profiles/templatetags/new_relic.py
+++ profiles/templatetags/new_relic.py
@@ -5,21 +5,16 @@ from django.templatetags.static import static
 
 register = template.Library()
 
+
 @register.simple_tag
 def new_relic():
     app_name = os.getenv("NEW_RELIC_APP_NAME", "")
     new_relic_script = ""
-    if app_name not in ["HC_Portal_Staging","HC_Portal_Production"]: 
+    if app_name not in ["HC_Portal_Staging", "HC_Portal_Production"]:
         return ""
     elif app_name == "HC_Portal_Staging":
         new_relic_script = "js/new_relic_staging.js"
     else:
         new_relic_script = "js/new_relic_production"
-    
-    return html.format_html("<script src='{}'></script>",
-    static(new_relic_script)
-    )
 
-
-
-    
+    return html.format_html("<script src='{}'></script>", static(new_relic_script))
