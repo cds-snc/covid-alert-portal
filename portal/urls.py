e5ae11c2524f6fef09bb2e66b20b0fa72a1a5c57
Pierre Paul Lefebvre <info@pierre-paul.com>
2020-07-23 14:00:14 -0400

    Updating translations and site title

--- portal/urls.py
+++ portal/urls.py
@@ -3,12 +3,11 @@ from django.urls import path, include, re_path
 from django.http import HttpResponse
 from django.conf.urls.i18n import i18n_patterns
 
-from django.utils.translation import gettext_lazy as _lz
-
 from .admin import Admin2FASite
 
 admin.site.__class__ = Admin2FASite
-admin.site.site_header = _lz("COVID Health portal")
+admin.site.site_header = "COVID Health Portal administration | Administration du Portail Alerte COVID"
+admin.site.site_title = admin.site.site_header
 
 urlpatterns = [
     re_path(
