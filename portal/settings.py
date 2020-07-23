5acf4dce9338eb37e772b0dfc9bf596df7e50641
Pierre Paul Lefebvre <info@pierre-paul.com>
2020-07-22 22:07:07 -0400

    Removing the extra , to keep black AND flake8 happy

--- portal/settings.py
+++ portal/settings.py
@@ -101,7 +101,7 @@ ROOT_URLCONF = "portal.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "portal", "templates"), ],
+        "DIRS": [os.path.join(BASE_DIR, "portal", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
