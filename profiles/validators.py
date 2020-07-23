80a44a858eb13602f331a45b5cf2faadfe422c55
Paul Craig <paul_craig_16@hotmail.com>
2020-07-22 20:07:56 -0400

    Add list of banned passwords
    To generate the list of banned passwords, I took Mark's list of [the NCSC's top 100k bad passwords](https://www.ncsc.gov.uk/static-assets/documents/PwnedPasswordsTop100k.txt) and then only included the ones that were 12 characters or more.
    
    It also flags any terms related to Coronavirus or the Healthcare Portal itself.
    
    Also added the UserAttributeSimilarityValidator, which will flag passwords that are too close to the person's name or email.
    
    Doesn't add the "NumericPasswordValidator" as common numeric passwords are already blocked by the list of banned passwords (eg, "111111111111").
    
    From Mark:
    
    > UserAttributeSimilarityValidator - I'd enable this. Gives us some element of customization that I was discussing.
    
    > MinimumLengthValidator - already enabled
    
    > CommonPasswordValidator - This is the banned password functionality. It looks like it actually has a 20k list baked in from haveibeenpwnd already. I recommend replacing this list with the 100k list from NCSC that I provided, tailored to remove anything with fewer than 12 characters. Feel free to add to the list if you feel that there are particular bad passwords that are 12+ characters. If you want me to provide this list, let me know as I think I have it somewhere from a previous project, though this would likely not really save you much time. I guess it would be of some benefit if attackers were unaware that we were implementing a banned password list, and better, if they were not able to determine which passwords were on that list. If you have thoughts on how to hide these aspects, go for it, however implementing this feature in the open, even with the banned list being public knowledge, is better than not implementing it at all. An attacker might tailor their wordlist for a guessing attack to remove the known banned passwords in order to improve their guesses. However, if they see that banned password functionality is implemented, I'm sure they would be able to take a good guess at what's on the banned password list.
    
    > NumericPasswordValidator - this is a good idea too. Turn this on and passwords like "111111111111" will be blocked :-)
    
    > If there's a way to ban any password that includes "covid" "coronovirus", "covid alert" covidalert" "corono virus", and anything else along these lines that the team can think of, it would be advisable to do so. Perhaps it can be accomplished using this functionality: https://docs.djangoproject.com/en/3.0/topics/auth/passwords/#writing-your-own-validator
    
    - Notify banned list: https://github.com/cds-snc/notification-admin/blob/master/app/main/_blacklisted_passwords.py
    - NCSC 100k top bad passwords: https://www.ncsc.gov.uk/static-assets/documents/PwnedPasswordsTop100k.txt
    - Included validators: https://docs.djangoproject.com/en/3.0/topics/auth/passwords/#module-django.contrib.auth.password_validation

---
+++ profiles/validators.py
@@ -0,0 +1,22 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
+from .passwords._banned_passwords import banned_passwords, banned_partial_passwords
+
+
+class BannedPasswordValidator(object):
+    def validate(self, password, user=None):
+        if password in banned_passwords:
+            raise ValidationError(
+                _("Your password can’t be a commonly-used sequence"),
+                code="password_banned",
+            )
+
+        for partial in banned_partial_passwords:
+            if partial.lower() in password.lower():
+                raise ValidationError(
+                    _("Your password can’t contain terms related to COVID-19"),
+                    code="password_banned_partial",
+                )
+
+    def get_help_text(self):
+        return _("Your password can’t be a commonly-used sequence.")
