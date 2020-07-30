3ad3a1993002d1ea88320a876ccfebd8209d4d5d
Pierre Paul Lefebvre <info@pierre-paul.com>
2020-07-30 11:22:01 -0400

    Removing extra print

--- profiles/templatetags/site_name.py
+++ profiles/templatetags/site_name.py
@@ -7,6 +7,4 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def site_name(context):
     request = context.get("request")
-    f = get_site_name(request) + ": "
-    print(f)
-    return f
+    return get_site_name(request) + ": "
