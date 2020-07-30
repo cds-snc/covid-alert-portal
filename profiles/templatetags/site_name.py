607bc6105e91fc6e78f60c4864ea8ae651c5767b
Pierre Paul Lefebvre <info@pierre-paul.com>
2020-07-30 11:15:46 -0400

    Concat site name with default translated subject

--- profiles/templatetags/site_name.py
+++ profiles/templatetags/site_name.py
@@ -7,4 +7,6 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def site_name(context):
     request = context.get("request")
-    return get_site_name(request) + ": "
+    f = get_site_name(request) + ": "
+    print(f)
+    return f
