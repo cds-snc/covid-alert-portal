2168a7bf6ec7667f075255a542cca381de469617
Pierre Paul Lefebvre <info@pierre-paul.com>
2020-07-31 22:44:51 -0400

    removing useless imports..

--- portal/context_processors.py
+++ portal/context_processors.py
@@ -1,12 +1,7 @@
 from django.contrib.messages.api import get_messages
-from django.contrib.messages.constants import DEFAULT_LEVELS
 
 
 def logout_messages(request):
-    """
-    Return a lazy 'messages' context variable as well as
-    'DEFAULT_MESSAGE_LEVELS'.
-    """
     messages = get_messages(request)
     messages = [message for message in messages if message.extra_tags == "logout"]
     return {
