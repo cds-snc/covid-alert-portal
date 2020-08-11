207f8e969c3e3977c018b97e2ea88c0e0e086a29
Paul Craig <paul_craig_16@hotmail.com>
2020-08-11 11:38:59 -0400

    Redirect to start page if already logged in
    If not logged in, we will see the login page first.

--- profiles/urls.py
+++ profiles/urls.py
@@ -9,7 +9,7 @@ from . import forms
 
 
 urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="login")),
+    path("", RedirectView.as_view(pattern_name="start")),
     path("invite/", views.InvitationView.as_view(), name="invite"),
     path("invite/list/", views.InvitationListView.as_view(), name="invitation_list"),
     path(
