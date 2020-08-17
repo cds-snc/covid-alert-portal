c82c95ee241f18d2a84b77d4936b012524949a2c
Pierre Paul Lefebvre <info@pierre-paul.com>
2020-08-17 16:27:51 -0400

    Send user to switch-language url that sets the language cookie properly.

--- profiles/templatetags/language_switcher.py
+++ profiles/templatetags/language_switcher.py
@@ -11,19 +11,3 @@ def get_lang_title(lang, langEn, langFr):
         return langFr.get("name_local").title()
     else:
         return langEn.get("name_local").title()
-
-
-@register.simple_tag(takes_context=True)
-def get_href(context, lang):
-    path = context.get("request").get_full_path()
-    root_domain = ""
-    translated_lang = "fr" if lang == "en" else "en"
-
-    if settings.URL_DUAL_DOMAINS:
-        root_domain = (
-            settings.URL_FR_PRODUCTION
-            if translated_lang == "fr"
-            else settings.URL_EN_PRODUCTION
-        )
-
-    return root_domain + translate_url(path, translated_lang)
