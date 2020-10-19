4608b6ea78b486d73ba9309ec5d4873bcd8f2d23
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-10-19 16:03:12 -0400

    Security Report v2 (#293)
    * Remove unused metrics
    
    * Add super_admin, admin, and staff widgets

--- config/terraform/aws/dashboard.tf
+++ config/terraform/aws/dashboard.tf
@@ -4,8 +4,64 @@ resource "aws_cloudwatch_dashboard" "ocio_report" {
 
   dashboard_body = <<EOF
 {
-    "start": "-P28D",
+    "start": "-P7D",
     "widgets": [
+        {
+            "type": "log",
+            "x": 0,
+            "y": 29,
+            "width": 15,
+            "height": 6,
+            "properties": {
+                "query": "SOURCE 'covidportal_staging' | fields @message\n| filter @message like /HTTP\\/1.1 400/ and @message not like /\\/status/\n| parse @message \"() {*} [*] * * => generated\" toss1, toss2, method, url\n| filter url != \"/\"\n| stats concat(method,\" \", url) as path, count(path) as attempts by path\n| sort by attempts desc",
+                "region": "ca-central-1",
+                "stacked": false,
+                "title": "Attempts that passed WAF but returned HTML Error 400 from COVID Alert Portal",
+                "view": "table"
+            }
+        },
+        {
+            "type": "log",
+            "x": 0,
+            "y": 13,
+            "width": 12,
+            "height": 9,
+            "properties": {
+                "query": "SOURCE 'covidportal_staging' | fields @timestamp, @message\n| filter @message like /LOGGING user_count/\n| parse @message '* LOGGING user_count province: \"*\" super_admins: * admins: * staff: *' logtime, province, super_admins, admins, staff\n| sort @timestamp desc\n| limit 15\n| display province, super_admins, admins, staff\n\n\n",
+                "region": "ca-central-1",
+                "stacked": false,
+                "title": "User type by Province",
+                "view": "table"
+            }
+        },
+        {
+            "type": "log",
+            "x": 15,
+            "y": 23,
+            "width": 9,
+            "height": 6,
+            "properties": {
+                "query": "SOURCE 'covidportal_staging' | fields @message\n| filter @message like /LOGIN login_type:login/\n| parse @message \"LOGIN login_type:login user_id:* remote_ip:*\" as userID, remoteIP\n| stats count_distinct(userID) as NumberOfUsers by remoteIP\n| filter (NumberOfUsers>1)",
+                "region": "ca-central-1",
+                "stacked": false,
+                "title": "Multiple Users per IP",
+                "view": "table"
+            }
+        },
+        {
+            "type": "log",
+            "x": 0,
+            "y": 10,
+            "width": 6,
+            "height": 3,
+            "properties": {
+                "query": "SOURCE 'covidportal_staging' | fields @message\n| filter @message like /model:profiles.healthcareuser/\n| parse @message \"CRUD event_type:* model:profiles.healthcareuser\" as action\n| stats sum(strcontains(action, \"CREATE\")) as CreatedUsers, sum(strcontains(action,\"DELETE\")) as DeletedUsers\n",
+                "region": "ca-central-1",
+                "stacked": false,
+                "title": "Total User Modification Actiivty",
+                "view": "table"
+            }
+        },
         {
             "type": "metric",
             "x": 0,
@@ -73,7 +129,7 @@ resource "aws_cloudwatch_dashboard" "ocio_report" {
         {
             "type": "metric",
             "x": 0,
-            "y": 14,
+            "y": 23,
             "width": 9,
             "height": 6,
             "properties": {
@@ -96,7 +152,7 @@ resource "aws_cloudwatch_dashboard" "ocio_report" {
         {
             "type": "text",
             "x": 0,
-            "y": 13,
+            "y": 22,
             "width": 24,
             "height": 1,
             "properties": {
@@ -104,23 +160,9 @@ resource "aws_cloudwatch_dashboard" "ocio_report" {
             }
         },
         {
-            "type": "log",
-            "x": 15,
-            "y": 14,
-            "width": 9,
-            "height": 6,
-            "properties": {
-                "query": "SOURCE 'covidportal_staging' | fields @message\n| filter @message like /LOGIN login_type:login/\n| parse @message \"LOGIN login_type:login user_id:* remote_ip:*\" as userID, remoteIP\n| stats count_distinct(userID) as NumberOfUsers by remoteIP\n| filter (NumberOfUsers>1)",
-                "region": "ca-central-1",
-                "stacked": false,
-                "title": "Multiple Users per IP",
-                "view": "table"
-            }
-        },
-        {
             "type": "metric",
             "x": 9,
-            "y": 14,
+            "y": 23,
             "width": 6,
             "height": 6,
             "properties": {
@@ -136,31 +178,32 @@ resource "aws_cloudwatch_dashboard" "ocio_report" {
             }
         },
         {
-            "type": "log",
+            "type": "text",
             "x": 0,
-            "y": 10,
-            "width": 6,
+            "y": 0,
+            "width": 24,
             "height": 3,
             "properties": {
-                "query": "SOURCE 'covidportal_staging' | fields @message\n| filter @message like /model:profiles.healthcareuser/\n| parse @message \"CRUD event_type:* model:profiles.healthcareuser\" as action\n| stats sum(strcontains(action, \"CREATE\")) as CreatedUsers, sum(strcontains(action,\"DELETE\")) as DeletedUsers\n",
-                "region": "ca-central-1",
-                "stacked": false,
-                "title": "User Activity",
-                "view": "table"
+                "markdown": "\n# Dashboard Generation Indicator\nWhen the majority of the bars in the graph below are at or near the '100' value the dashboard is completely loaded.\n\nThis amount of time is required to ensure that the 'User Activity', 'Multiple Users / IP', and 'Attempts that succeeded WAF but were blocked by COVID Alert Portal' have enough time to completely load.\n"
             }
         },
         {
-            "type": "log",
-            "x": 0,
-            "y": 20,
-            "width": 15,
-            "height": 6,
+            "type": "metric",
+            "x": 12,
+            "y": 13,
+            "width": 12,
+            "height": 9,
             "properties": {
-                "query": "SOURCE 'covidportal_staging' | fields @message\n| filter @message like /HTTP\\/1.1 400/ and @message not like /\\/status/\n| parse @message \"() {*} [*] * * => generated\" toss1, toss2, method, url\n| filter url != \"/\"\n| stats concat(method,\" \", url) as path, count(path) as attempts by path\n| sort by attempts desc",
-                "region": "ca-central-1",
+                "metrics": [
+                    [ "covidportal", "UsersLogin", { "id": "m1", "stat": "Sum", "label": "UserLogins", "period": 3600 } ],
+                    [ { "expression": "ANOMALY_DETECTION_BAND(m1, 2)", "label": "AnomalyBand", "id": "e1", "region": "ca-central-1" } ]
+                ],
+                "view": "timeSeries",
                 "stacked": false,
-                "title": "Attempts that succeeded WAF but were blocked by COVID Alert Portal",
-                "view": "table"
+                "region": "ca-central-1",
+                "stat": "Average",
+                "period": 300,
+                "title": "User Login Anomolay Detection Band"
             }
         },
         {
@@ -175,16 +218,6 @@ resource "aws_cloudwatch_dashboard" "ocio_report" {
                 "stacked": false,
                 "title": "Report Generation Progress Indicator",
                 "view": "bar"
-            }
-        },
-        {
-            "type": "text",
-            "x": 0,
-            "y": 0,
-            "width": 24,
-            "height": 3,
-            "properties": {
-                "markdown": "\n# Dashboard Generation Indicator\nWhen the majority of the bars in the graph below are at or near the '100' value the dashboard is completely loaded.\n\nThis amount of time is required to ensure that the 'User Activity', 'Multiple Users / IP', and 'Attempts that succeeded WAF but were blocked by COVID Alert Portal' have enough time to completely load.\n"
             }
         }
     ]
