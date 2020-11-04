fd2622691d438cf6658f0d5a66b9fc261e2b0d99
Paul Craig <paul_craig_16@hotmail.com>
2020-11-04 11:19:27 -0500

    Add tests for backup codes
    - test users can view their own codes
    - test that there are 5 codes generated
    - test that generating new codes invalidates the old ones
    - test for the "visually-hidden" vs "aria-hidden" layout

--- profiles/tests.py
+++ profiles/tests.py
@@ -1267,3 +1267,72 @@ class ProfileEditView(AdminUserTestCase):
         self.assertEqual(response.status_code, 302)
         user = HealthcareUser.objects.get(pk=self.user.id)
         self.assertEqual(user.phone_number, number)
+
+
+from waffle.models import Switch
+from django_otp.plugins.otp_static.models import StaticDevice
+
+
+class SecurityCodeView(AdminUserTestCase):
+    def setUp(self):
+        super().setUp(is_admin=True)
+        Switch.objects.create(name="SECURITY_CODE", active=True)
+
+    def test_user_can_view_security_codes_row_on_account_page(self):
+        self.login()
+        response = self.client.get(reverse("user_profile", kwargs={"pk": self.user.id}))
+        self.assertEqual(response.status_code, 200)
+        ## no security codes link present
+        self.assertContains(response, '<a href="/en/profiles/security-codes">')
+
+    def test_user_can_generate_5_security_codes(self):
+        self.login()
+        response = self.client.get(reverse("security_code_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<h1>Security Codes</h1>")
+
+        device = StaticDevice.objects.get(user__id=self.user.id)
+        self.assertEqual(len(device.token_set.all()), 5)
+
+    def test_old_security_codes_invalid_if_new_security_codes_generated(self):
+        self.login()
+        response = self.client.get(reverse("security_code_list"))
+        self.assertEqual(response.status_code, 200)
+
+        old_codes = [
+            t.token
+            for t in StaticDevice.objects.get(user__id=self.user.id).token_set.all()
+        ]
+
+        response = self.client.get(reverse("security_code_list"))
+        self.assertEqual(response.status_code, 200)
+        new_codes = [
+            t.token
+            for t in StaticDevice.objects.get(user__id=self.user.id).token_set.all()
+        ]
+
+        for code in old_codes:
+            self.assertNotIn(code, new_codes)
+
+    def test_user_codes_HTML(self):
+        self.login()
+        response = self.client.get(reverse("security_code_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "<h1>Security Codes</h1>")
+        device = StaticDevice.objects.get(user__id=self.user.id)
+
+        tokens = [t.token for t in device.token_set.all()]
+
+        for token in tokens:
+            self.assertContains(
+                response,
+                '<span class="visually-hidden">{}</span>'.format(
+                    " ".join(token.upper())
+                ),
+            )
+            self.assertContains(
+                response,
+                '<span aria-hidden="true"><span>{}</span><span>{}</span>'.format(
+                    token[:4], token[-4:]
+                ),
+            )
