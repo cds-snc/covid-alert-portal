77db1a5da202b398dd5226ecb16b014f2dad4d10
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-11-25 14:19:27 -0500

    Completed tests

--- announcements/models.py
+++ announcements/models.py
@@ -21,21 +21,23 @@ class Announcement(models.Model):
     title_fr = models.CharField(_("Title in French"), max_length=100, blank=False)
     content_en = models.TextField(_("English Content"), blank=True)
     content_fr = models.TextField(_("French Content"), blank=True)
-    display = models.BooleanField(_("show_to_users"), default=False)
+    display = models.BooleanField(_("Show to users"), default=False)
     level = models.CharField(max_length=7, choices=LEVEL_CHOICES, default="info")
-    creation_date = models.DateTimeField(_("creation_date"), auto_now=True)
-    site_wide = models.BooleanField(_("site wide"), default=False)
+    creation_date = models.DateTimeField(_("Creation date"), auto_now=True)
+    site_wide = models.BooleanField(_("Site Wide"), default=False)
     for_user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         null=True,
         blank=True,
         default=None,
-        verbose_name=_("for_user"),
+        verbose_name=_("For User"),
         on_delete=models.CASCADE,
     )
-    dismissable = models.BooleanField(_("dismissable"), default=False)
-    publish_start = models.DateField(_("publish_start"), default=datetime.date.today)
-    publish_end = models.DateField(_("publish_end"), blank=True, null=True)
+    dismissable = models.BooleanField(_("Dismissable"), default=False)
+    publish_start = models.DateField(
+        _("Publish start date"), default=datetime.date.today
+    )
+    publish_end = models.DateField(_("Publish end date"), blank=True, null=True)
 
     def dismiss_url(self):
         if self.dismissable:
--- announcements/tests.py
+++ announcements/tests.py
@@ -54,3 +54,62 @@ class AnnoncementsSiteWide(AdminUserTestCase):
         # Page 3
         response = self.client.post(reverse("key"))
         self.assertContains(response, '<div class="title">Test for Site Wide</div>')
+
+    def test_to_ensure_user_scoped_is_only_visible_to_that_user(self):
+        Announcement.objects.create(
+            title_en="Test for User 1 Only",
+            title_fr="Test pour utilisateur 1 seulement",
+            level="info",
+            display=True,
+            for_user=self.user,
+        )
+        self.login()
+        response = self.client.get(reverse("start"))
+        self.assertContains(response, '<div class="title">Test for User 1 Only</div>')
+        self.client.logout()
+
+        # Second user can not see message
+        self.login(self.user2_credentials)
+        response = self.client.get(reverse("start"))
+        self.assertNotContains(
+            response, '<div class="title">Test for User 1 Only</div>'
+        )
+
+    def test_that_only_active_announcements_are_displayed(self):
+        Announcement.objects.create(
+            title_en="Test for Site Wide - Not Enabled",
+            title_fr="Voici un test Site Wide - Non active",
+            level="info",
+            display=False,
+            site_wide=True,
+        )
+
+        self.login()
+        response = self.client.get(reverse("start"))
+        self.assertContains(response, '<div class="title">Test for Site Wide</div>')
+        self.assertNotContains(
+            response, '<div class="title">Test for Site Wide - Not Enabled</div>'
+        )
+
+    def test_to_ensure_announcement_is_dismissable(self):
+        Announcement.objects.create(
+            title_en="Test for Site Wide - Not Enabled",
+            title_fr="Voici un test Site Wide - Non active",
+            level="info",
+            display=True,
+            site_wide=True,
+            dismissable=True,
+        )
+        self.login()
+        response = self.client.get(reverse("start"))
+        self.assertContains(
+            response, '<span class="dismiss" aria-label="Dismiss">&times</span>'
+        )
+
+    def test_to_ensure_announcement_is_not_dismissable(self):
+
+        self.login()
+        response = self.client.get(reverse("start"))
+        self.assertNotContains(
+            response, '<span class="dismiss" aria-label="Dismiss">&times</span>'
+        )
