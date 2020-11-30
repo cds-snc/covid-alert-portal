4fd5518f8b6b23fe490755b61623e95e3e24b77f
Bryan Robitaille <bryan.robitaille.work@gmail.com>
2020-11-30 10:05:51 -0500

    Add signal for when user is deleted

--- backup_codes/signals.py
+++ backup_codes/signals.py
@@ -7,21 +7,30 @@ from announcements.models import Announcement
 from .views import _remove_low_on_codes_notification
 import waffle
 
+from profiles.models import HealthcareUser
+
+@receiver(post_delete, sender=HealthcareUser)
+def remove_any_outstanding_announcements_to_be_created(sender, instance, **kwargs):
+    #  Remove any announcements for the user that were just created becuase of low code signal
+    if (waffle.switch_is_active("BACKUP_CODE")):
+        Announcement.objects.filter(for_user=instance).delete()
 
 @receiver(post_delete, sender=StaticToken)
 def add_low_on_tokens_notification(sender, instance, **kwargs):
-    if waffle.switch_is_active("BACKUP_CODE"):
+    if (waffle.switch_is_active("BACKUP_CODE")):
+        for_user = HealthcareUser.objects.get(id=instance.device.user.id)
         users_static_codes_remaining = instance.device.token_set.all().count()
+
         if users_static_codes_remaining <= 1:
             # Remove existing notification for only 1 code remaining if it exists
-            _remove_low_on_codes_notification(instance.device.user)
+            _remove_low_on_codes_notification(for_user)
 
             Announcement.objects.create(
                 title_en=f"You have {users_static_codes_remaining} security code{'s' if users_static_codes_remaining == 0 else ''} remaining.",
                 title_fr=f"Il vous reste {users_static_codes_remaining} code{'s' if users_static_codes_remaining == 0 else ''} de sécurité.",
-                content_en=f"To get more codes, visit <a href='/profiles/{instance.device.user.id}'>Your Account</a>",
-                content_fr=f"Pour obtenir plus de codes, visitez <a href='/profiles/{instance.device.user.id}'>Votre Compte</a>",
+                content_en=f"To get more codes, visit <a href='/profiles/{for_user.id}'>Your Account</a>",
+                content_fr=f"Pour obtenir plus de codes, visitez <a href='/profiles/{for_user.id}'>Votre Compte</a>",
                 level="info",
-                for_user=instance.device.user,
+                for_user=for_user,
                 is_active=True,
             )
