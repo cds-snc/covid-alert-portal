25c131f8c3e17050858684bf9c4e6d78750511b2
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-01-29 16:24:38 -0500

    Add Location model, migration, and admin config

--- register/admin.py
+++ register/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 
-from .models import Registrant
+from .models import Registrant, Location
 
 
 # Register your models here.
 class RegistrantAdmin(admin.ModelAdmin):
     list_display = ["id", "name", "email"]
 
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ["id", "name", "city", "province"]
 
 admin.site.register(Registrant, RegistrantAdmin)
+admin.site.register(Location, LocationAdmin)
--- register/models.py
+++ register/models.py
@@ -14,3 +14,18 @@ class Registrant(models.Model):
 
     def __str__(self):  # new
         return "{} ({})".format(self.name, self.email)
+
+class Location(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    category = models.CharField(max_length=200, verbose_name=_("Location category"))
+    name = models.CharField(max_length=200, verbose_name=_("Location name"))
+    address = models.CharField(max_length=200, verbose_name=_("Address line 1"))
+    address_2 = models.CharField(max_length=200, verbose_name=_("Address line 2"))
+    city = models.CharField(max_length=100, verbose_name=_("City"))
+    province = models.CharField(max_length=100, verbose_name=_("Province"))
+    postal_code = models.CharField(max_length=10, verbose_name=_("Postal code"))
+    contact_email = models.EmailField(verbose_name=_("Email address"), max_length=255)
+    contact_phone = models.CharField(max_length=255, verbose_name=_("Contact phone"))
+
+    def __str__(self):
+        return "{} - {}, {}, {}, {}".format(self.name, self.address, self.city, self.province, self.postal_code)
