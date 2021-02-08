c57abb3f391e70671280f9fc7be576898d422334
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-02-08 12:38:42 -0500

    Add unique short_code to Location model and generate on create

--- register/models.py
+++ register/models.py
@@ -19,6 +19,9 @@ class Registrant(models.Model):
 
 class Location(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    short_code = models.CharField(
+        max_length=8, unique=True, null=True, verbose_name=_("Short code")
+    )
     category = models.CharField(max_length=200, verbose_name=_("Location category"))
     name = models.CharField(max_length=200, verbose_name=_("Location name"))
     address = models.CharField(max_length=200, verbose_name=_("Address line 1"))
@@ -28,6 +31,16 @@ class Location(models.Model):
     postal_code = models.CharField(max_length=10, verbose_name=_("Postal code"))
     contact_email = models.EmailField(verbose_name=_("Email address"), max_length=255)
     contact_phone = PhoneNumberField(blank=True)
+
+    # Override save method to generate a unique short code for poster
+    def save(self, *args, **kwargs):
+        if not self.short_code:
+            while True:
+                short_code = uuid4().hex[:8].upper()
+                if not Location.objects.filter(short_code=short_code).exists():
+                    break
+            self.short_code = short_code
+        super(Location, self).save(*args, **kwargs)
 
     def __str__(self):
         return "{} - {}, {}, {}, {}".format(
