ccb2ac269e2c943edf382bef8ddf97c428417fbd
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-02-09 14:18:35 -0500

    fix linting errors

--- register/utils.py
+++ register/utils.py
@@ -1,5 +1,7 @@
-import random, string
+import random
+import string
 
-# Will generate a random string with 62^length possible combinations
+
+# Will generate a random alphanumeric string with 62^length possible combinations
 def generate_random_key(length=8):
     return "".join(random.choices(string.ascii_letters + string.digits, k=length))
