546e9ded680e9bb6ae77aa2d8bffc546fddd32e6
Andrej Vasilj <andrej.vasilj@cds-snc.ca>
2021-02-12 12:14:39 -0500

    fix attr error

--- config/terraform/aws/certificates.tf
+++ config/terraform/aws/certificates.tf
@@ -74,9 +74,10 @@ locals {
 resource "aws_route53_record" "cert_validation2" {
   for_each = {
     for dvo in aws_acm_certificate.covidportal_certificate2.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
+      domain_name = dvo.domain_name
+      name        = dvo.resource_record_name
+      record      = dvo.resource_record_value
+      type        = dvo.resource_record_type
     }
   }
 
