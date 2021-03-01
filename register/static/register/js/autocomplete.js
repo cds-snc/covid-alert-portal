6faf72f06b02af92877843378a72c0b09a3b346a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-03-01 16:31:53 -0500

    add accessible-autocomplete lib and first cut at hooking it up

---
+++ register/static/register/js/autocomplete.js
@@ -0,0 +1,102 @@
+const url = 'http://ws1.postescanada-canadapost.ca/AddressComplete/Interactive/Find/v2.10/json3.ws';
+const key = 'BC76-EY79-GM26-BZ52'
+
+var results = [];
+
+function hinter(query, populateResults) {
+    var params = '';
+    params += "&Key=" + encodeURIComponent(key);
+    params += "&SearchTerm=" + encodeURIComponent(query);
+    params += "&Country=" + encodeURIComponent('CAN')
+
+    var http = new XMLHttpRequest();
+    http.open('POST', url, true);
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {
+        if(http.readyState == 4 && http.status == 200) {
+            var response = JSON.parse(http.responseText);
+            // Test for an error
+            if (response.Items.length == 1 && typeof(response.Items[0].Error) != "undefined") {
+                // Show the error message
+                console.log(response.Items[0].Description);
+            } else {
+                // Check if there were any items found
+                if (response.Items.length == 0)
+                    alert("Sorry, there were no results");
+                else {
+                    console.log(response.Items)
+                    var results = response.Items.map(function(result) {
+                        return {
+                            'name': result['Text'],
+                            'id': result['Id']
+                        }
+                    })
+                    console.log(results);
+                    populateResults(results)
+                }
+        }
+        }
+    }
+    http.send(params);
+}
+
+
+accessibleAutocomplete({
+    element: document.querySelector('#my-autocomplete-container'),
+    id: 'my-autocomplete', // To match it to the existing <label>.
+    source: hinter,
+    onConfirm: function(confirmed) {
+        console.log(confirmed)
+    },
+    templates: {
+        suggestion: function(item) {
+            return item.name
+        },
+    }
+})
+
+  // var ac_input = document.getElementById('my-autocomplete');
+  //   ac_input.addEventListener("keyup", function(event){hinter(event)});
+
+
+  /* SAMPLE FROM CANADA POST
+var params = '';
+    params += "&Key=" + encodeURIComponent(Key);
+    params += "&SearchTerm=" + encodeURIComponent(SearchTerm);
+    params += "&LastId=" + encodeURIComponent(LastId);
+    params += "&SearchFor=" + encodeURIComponent(SearchFor);
+    params += "&Country=" + encodeURIComponent(Country);
+    params += "&LanguagePreference=" + encodeURIComponent(LanguagePreference);
+    params += "&MaxSuggestions=" + encodeURIComponent(MaxSuggestions);
+    params += "&MaxResults=" + encodeURIComponent(MaxResults);
+var http = new XMLHttpRequest();
+http.open('POST', url, true);
+http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+http.onreadystatechange = function() {
+  if(http.readyState == 4 && http.status == 200) {
+      var response = JSON.parse(http.responseText);
+      // Test for an error
+      if (response.Items.length == 1 && typeof(response.Items[0].Error) != "undefined") {
+        // Show the error message
+        alert(response.Items[0].Description);
+      }
+      else {
+        // Check if there were any items found
+        if (response.Items.length == 0)
+            alert("Sorry, there were no results");
+        else {
+            // PUT YOUR CODE HERE
+            //FYI: The output is an array of key value pairs (e.g. response.Items[0].Id), the keys being:
+            //Id
+            //Text
+            //Highlight
+            //Cursor
+            //Description
+            //Next
+        }
+    }
+  }
+}
+http.send(params);
+*/
