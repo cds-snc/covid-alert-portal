55e275f215a83c3038616bc60955477626a4f2ae
Stephen McMurtry <smcmurtry@gmail.com>
2021-03-12 16:11:09 -0700

    removed unused imports

--- outbreaks/forms.py
+++ outbreaks/forms.py
@@ -1,11 +1,9 @@
 from django import forms
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
-from django.conf import settings
 from portal.widgets import CDSRadioWidget
 from portal.forms import HealthcareBaseForm
 from datetime import datetime
-import pytz
 from dateutil import tz
 
 severity_choices = [
