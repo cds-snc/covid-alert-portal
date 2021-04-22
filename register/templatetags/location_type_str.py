3f634d3fa7cde411ebade6d4ca479d3938237b86
Dorian Pruski <dorian.pruski@cds-snc.ca>
2021-04-22 11:10:18 -0600

    black reformat

--- register/templatetags/location_type_str.py
+++ register/templatetags/location_type_str.py
@@ -1,26 +1,36 @@
 from django import template
 from django.utils.translation import gettext_lazy as _
-from ..forms import type_establishment, type_event, type_event_or_establishment, location_category_type_map
+from ..forms import (
+    type_establishment,
+    type_event,
+    type_event_or_establishment,
+    location_category_type_map,
+)
+
 register = template.Library()
 
 
 @register.simple_tag
-def location_type_str(location_category, prefix, * args, **kwargs):
+def location_type_str(location_category, prefix, *args, **kwargs):
     """
     Returns whole string based on type of location, event or establishment
     """
-    if prefix == 'address':
+    if prefix == "address":
         if location_category_type_map[location_category] == type_establishment:
-            return _('Address of establishment')
+            return _("Address of establishment")
         elif location_category_type_map[location_category] == type_event:
-            return _('Address of event')
-        elif location_category_type_map[location_category] == type_event_or_establishment:
-            return _('Address of establishment or event')
-    elif prefix == 'name':
+            return _("Address of event")
+        elif (
+            location_category_type_map[location_category] == type_event_or_establishment
+        ):
+            return _("Address of establishment or event")
+    elif prefix == "name":
         if location_category_type_map[location_category] == type_establishment:
-            return _('Name of establishment')
+            return _("Name of establishment")
         elif location_category_type_map[location_category] == type_event:
-            return _('Name of event')
-        elif location_category_type_map[location_category] == type_event_or_establishment:
-            return _('Name of establishment or event')
+            return _("Name of event")
+        elif (
+            location_category_type_map[location_category] == type_event_or_establishment
+        ):
+            return _("Name of establishment or event")
     raise ValueError("Location category or prefix not valid for template tag filter.")
