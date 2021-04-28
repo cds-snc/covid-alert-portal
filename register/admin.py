b3399f8abffa9557d78df352a119872b7764d598
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-04-28 15:08:12 -0400

    add Location Summary admin panel

--- register/admin.py
+++ register/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
 from .models import Registrant, Location, LocationSummary
+from django.db.models import Count
 
 
 @admin.register(Registrant)
@@ -16,3 +17,41 @@ class LocationAdmin(admin.ModelAdmin):
         'province',
         'city',
     )
+
+
+@admin.register(LocationSummary)
+class LocationSummaryAdmin(admin.ModelAdmin):
+    change_list_template = 'admin/location_summary_change_list.html'
+    date_hierarchy = 'created'
+
+    class Media:
+        css = {
+            'all': ('css/admin.css',)
+        }
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(
+            request,
+            extra_context=extra_context,
+        )
+
+        try:
+            qs = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+
+        qs_province = qs.values('province').annotate(total=Count('province')).order_by('-total')
+        response.context_data['province_summary'] = list(qs_province)
+
+        response.context_data['province_summary_total'] = dict(
+            qs.aggregate(total=Count('province'))
+        )
+
+        qs_city = qs.values('city').annotate(total=Count('city')).order_by('-total')
+
+        response.context_data['city_summary'] = list(qs_city)
+        response.context_data['city_summary_total'] = dict(
+            qs.aggregate(total=Count('city'))
+        )
+        
+        return response
