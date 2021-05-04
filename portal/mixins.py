e78a085ef66c0eb034b0d39ef028731ede207c8d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-05-04 16:32:37 -0400

    formatting

--- portal/mixins.py
+++ portal/mixins.py
@@ -142,8 +142,8 @@ class ExportCsvMixin:
         meta = self.model._meta
         field_names = [field.name for field in meta.fields]
 
-        response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        response = HttpResponse(content_type="text/csv")
+        response["Content-Disposition"] = "attachment; filename={}.csv".format(meta)
         writer = csv.writer(response)
 
         writer.writerow(field_names)
@@ -153,4 +153,3 @@ class ExportCsvMixin:
         return response
 
     export_as_csv.short_description = "Export Selected"
-    
