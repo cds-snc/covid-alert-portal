783ef3c824c7997811c256848baac503db30d3c8
Dorian Pruski <dorian.pruski@cds-snc.ca>
2021-05-12 14:22:03 -0600

    working toggle with and without env var

--- google_analytics/templatetags/ga_id.py
+++ google_analytics/templatetags/ga_id.py
@@ -2,15 +2,6 @@ from django import template
 from django.conf import settings
 register = template.Library()
 
-if settings.GA_ID:
-    template = "includes/google_analytics.html"
-else:
-    template = None
-
-@register.inclusion_tag(template)
-def get_ga_script():
-    return
-
 @register.simple_tag
 def get_ga_id():
     return settings.GA_ID
