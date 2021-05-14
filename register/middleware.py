e5d0486bda8f2bd8409ad341e7c0a0d69c21739c
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-05-14 15:03:42 -0400

    formatting

--- register/middleware.py
+++ register/middleware.py
@@ -5,10 +5,9 @@ from basicauth.response import HttpResponseUnauthorized
 
 class BasicAuthMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        if 'status' in request.path:
+        if "status" in request.path:
             return None
 
         if not validate_request(request):
             return HttpResponseUnauthorized()
         return None
-    
