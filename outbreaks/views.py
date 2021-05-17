30c141515e8836a33e6c1319898abd96393d272f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-05-17 13:43:41 -0400

    set session var so link works

--- outbreaks/views.py
+++ outbreaks/views.py
@@ -592,6 +592,8 @@ class ExposureDetailsView(PermissionRequiredMixin, Is2FAMixin, TemplateView):
             context["map_link"] = "https://maps.google.com/?q=" + str(
                 notification.location
             )
+            
+            self.request.session["alert_location"] = notification.location.id.hex
         except Location.DoesNotExist:
             pass
         return context
