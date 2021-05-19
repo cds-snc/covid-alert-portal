bcf51a279c8bab708a4fdcec36ed7357491aa839
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-05-19 15:13:49 -0400

    formatting

--- register/forms.py
+++ register/forms.py
@@ -77,9 +77,15 @@ class LocationCategoryForm(HealthcareBaseForm, forms.Form):
 
 
 class LocationNameForm(HealthcareBaseForm, forms.Form):
-    name = forms.CharField(label="", max_length=65, error_messages={
-        "max_length": _("Your name is longer than the %(limit_value)d character limit.")
-    })
+    name = forms.CharField(
+        label="",
+        max_length=65,
+        error_messages={
+            "max_length": _(
+                "Your name is longer than the %(limit_value)d character limit."
+            )
+        },
+    )
 
 
 provinces = [
