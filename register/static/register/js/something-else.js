8fb3ab8aea78964bba311e6801efdc53d8e654bd
Dorian Pruski <dorianjp@users.noreply.github.com>
2021-05-20 20:58:48 -0600

    Change venue selection from radio to dropdown (#533)
    * changed radio buttons to dropdown
    * added venues, slight content changes

--- register/static/register/js/something-else.js
+++ register/static/register/js/something-else.js
@@ -1,22 +1,17 @@
 document.getElementById("id_category-category_description").parentElement.style.display = 'none'
 
-
-const radios = document.getElementsByName("category-category")
-
-for (var i = 0, len = radios.length; i < len; i++) {
-    if (radios[i].checked) {
-      if (radios[i].value === "other") {
-        document.getElementById("id_category-category_description").parentElement.style.display = 'block';
-      }
-    }
-    radios[i].onclick = function() {
-      if (this.checked && this.value === "other") {
-        document.getElementById("id_category-category_description").parentElement.style.display = 'block';
-      } else {
-        document.getElementById("id_category-category_description").value = "";
-        document.getElementById("id_category-category_description").parentElement.style.display = 'none';
-      }
-    };
+const categorySelect = document.getElementById('id_category-category');
+if (categorySelect.value === 'other'){
+  document.getElementById("id_category-category_description").parentElement.style.display = 'block';
+}
+categorySelect.onchange = function (e) {
+  if (this.value === 'other') {
+    document.getElementById("id_category-category_description").parentElement.style.display = 'block';
+  } else {
+    document.getElementById("id_category-category_description").value = "";
+    document.getElementById("id_category-category_description").parentElement.style.display = 'none';
   }
-
-
+}
+// Add optgroup to allow text zoom in mobile html control
+const optGroup = document.createElement("optgroup");
+categorySelect.append(optGroup);
