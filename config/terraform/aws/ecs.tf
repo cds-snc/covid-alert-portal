bba2a213105e24e0313cae8ea4fafa3e6eb468cd
Pat Heard <patrick.heard@cds-snc.ca>
2021-06-21 11:22:20 -0400

    feat: re-add FARGATE capacity provider to services
    Now that the cluster capacity provider has been properly set,
    the services and tasks can be updated as well.

--- config/terraform/aws/ecs.tf
+++ config/terraform/aws/ecs.tf
@@ -10,6 +10,14 @@ resource "aws_ecs_cluster" "covidportal" {
     value = "enabled"
   }
 
+  capacity_providers = ["FARGATE"]
+
+  default_capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight            = 1
+    base              = 2
+  }
+
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
   }
@@ -155,6 +163,12 @@ resource "aws_ecs_service" "covidportal" {
     container_port   = 8000
   }
 
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight            = 1
+    base              = 2
+  }  
+
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
   }
@@ -205,6 +219,12 @@ resource "aws_ecs_service" "qrcode" {
     container_name   = var.ecs_covid_portal_name
     container_port   = 8000
   }
+
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight            = 1
+    base              = 2
+  }  
 
   tags = {
     (var.billing_tag_key) = var.billing_tag_value
