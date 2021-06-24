6942901921e99aeefadeda8f050b524993e6f0f6
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-06-24 16:24:27 -0400

    Refactor github actions to work with WhiteNoise and to speed up CI

--- portal/settings.py
+++ portal/settings.py
@@ -280,7 +280,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "portal", "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 AUTH_USER_MODEL = "profiles.HealthcareUser"
 
