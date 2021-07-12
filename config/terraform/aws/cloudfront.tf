750bc2b82ee9716f04daa2980bd4c24a68ba96ed
Pat Heard <patrick.heard@cds-snc.ca>
2021-07-12 13:23:29 -0400

    feat: add CloudFront to QR code site
    This creates a CloudFront distribution for QR code that
    does the following:
    
    1. Cache all GET/HEAD requests, respecting response cache headers.
    2. Cache /static/* GET/HEAD requests for 1 week, replying on cache
    busting in asset filenames to keep cached items fresh.

--- config/terraform/aws/cloudfront.tf
+++ config/terraform/aws/cloudfront.tf
@@ -55,12 +55,110 @@ resource "aws_cloudfront_distribution" "maintenance_mode" {
     }
   }
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate_validation.cert.certificate_arn
+    acm_certificate_arn      = aws_acm_certificate.covidportal_certificate.arn
     minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
   }
 
+  price_class = "PriceClass_100"
+
   depends_on = [
     aws_s3_bucket.portal_maintenance_mode
   ]
+}
+
+###
+# AWS Cloudfront (CDN) - QR code
+###
+
+resource "aws_cloudfront_distribution" "qrcode" {
+  origin {
+    domain_name = aws_lb.qrcode.dns_name
+    origin_id   = aws_lb.qrcode.name
+
+    custom_header {
+      name  = "covidqrcode"
+      value = var.cloudfront_qrcode_custom_header
+    }
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  web_acl_id      = aws_wafv2_web_acl.qrcode_cdn_acl.arn
+
+  aliases = ["register.covid-hcportal.cdssandbox.xyz"]
+
+  # Cache GET/HEAD respecting cache-control headers (min_ttl = 0)
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "POST"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_lb.qrcode.name
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Referer"]
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 7200
+    compress               = true
+  }
+
+  # Cache `/static/*` assets for 1 week
+  # Cache busting has been added to the asset filenames to keep requests fresh
+  ordered_cache_behavior {
+    path_pattern     = "/static/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_lb.qrcode.name
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Referer"]
+      cookies {
+        forward = "all"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 604800
+    default_ttl            = 604800
+    max_ttl                = 604800
+    compress               = true
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "whitelist"
+      locations        = ["CA", "US"]
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.covidportal_certificate.arn
+    minimum_protocol_version = "TLSv1.2_2021"
+    ssl_support_method       = "sni-only"
+  }
+
+  logging_config {
+    include_cookies = false
+    bucket          = aws_s3_bucket.cloudfront_logs.bucket_domain_name
+    prefix          = "cloudfront"
+  }
+
+  depends_on = [aws_s3_bucket.cloudfront_logs]
 }
--- config/terraform/aws/route53.tf
+++ config/terraform/aws/route53.tf
@@ -77,9 +77,9 @@ resource "aws_route53_record" "qrcode" {
   }
 
   alias {
-    name                   = aws_lb.qrcode.dns_name
-    zone_id                = aws_lb.qrcode.zone_id
-    evaluate_target_health = true
+    name                   = aws_cloudfront_distribution.qrcode.domain_name
+    zone_id                = aws_cloudfront_distribution.qrcode.hosted_zone_id
+    evaluate_target_health = false
   }
 }
 
