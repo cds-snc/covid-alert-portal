943d6b0db56a9db987c42f571746f529ab395ee6
Pat Heard <patrick.heard@cds-snc.ca>
2021-07-19 07:46:15 -0400

    feat: add CloudFront to QR code site (#693)
    This creates a CloudFront distribution for QR code that
    does the following:
    
    1. By default, do not cache any requests.  This is because currently
    there are no response cache headers being set for dynamic content.
    
    2. Cache /static/* GET/HEAD requests for 1 week, replying on cache
    busting in asset filenames to keep cached items fresh.

--- config/terraform/aws/s3.tf
+++ config/terraform/aws/s3.tf
@@ -98,3 +98,45 @@ resource "aws_s3_bucket_policy" "web_distribution" {
   bucket = aws_s3_bucket.portal_maintenance_mode.id
   policy = data.aws_iam_policy_document.web_distribution.json
 }
+
+###
+# AWS S3 bucket - cloudfront log target
+###
+resource "aws_s3_bucket" "cloudfront_logs" {
+
+  # tfsec:ignore:AWS002 Logging not required in Staging
+  # tfsec:ignore:AWS077 Don't need to version these they should expire and are ephemeral
+
+  bucket = "covid-portal-${var.environment}-cloudfront-logs"
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 90
+    }
+  }
+
+  # awslogsdelivery account needs full control for cloudfront logging
+  # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html#AccessLogsBucketAndFileOwnership
+  grant {
+    id          = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+    type        = "CanonicalUser"
+    permissions = ["FULL_CONTROL"]
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "cloudfront_logs" {
+  bucket = aws_s3_bucket.cloudfront_logs.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
