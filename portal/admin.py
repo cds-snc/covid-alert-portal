279c5b0a4f1a34f433d31b76595962fefa955a0f
sylviamclaughlin <sylvia.mclaughlin@cds-snc.ca>
2021-08-03 15:39:13 -0700

    Updated translations for Terms of use

--- portal/admin.py
+++ portal/admin.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
 from django.contrib.auth.models import Group
+from django.conf import settings
 
 from waffle.models import Flag, Sample
 
 
 class Admin2FASite(AdminSite):
     name = "2faadmin"
-    login_template = "admin/login-disabled.html"
+    if not settings.DECOMMISSION:
+        login_template = "admin/login-disabled.html"
 
     def has_permission(self, request):
         # Let's make sure 2fa is enabled and confirmed on this user
-        return super().has_permission(request) and request.user.is_verified()
+        if settings.DECOMMISSION:
+            return super().has_permission(request)
+        else:
+            return super().has_permission(request) and request.user.is_verified()
 
 
 # unregister the Group model from admin.
